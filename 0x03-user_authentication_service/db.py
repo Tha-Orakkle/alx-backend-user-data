67905dbbe67d8f5707f35fa57b9a28c9e1d1532f
tha-orakkle <adegbiranayinoluwa.paul@yahoo.com>
2024-06-14 18:47:49 +0100

    update user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -2,9 +2,11 @@
 """DB module
 """
 from sqlalchemy import create_engine
+from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
 
 
 from user import Base, User
@@ -39,3 +41,28 @@ class DB:
         self._session.commit()
 
         return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """Find a user by arbitrary keyqord argument
+        """
+        for k in kwargs:
+            if k not in User.__table__.columns.keys():
+                raise InvalidRequestError
+
+        user = self._session.query(User).filter_by(**kwargs).first()
+
+        if not user:
+            raise NoResultFound
+
+        return user
+
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """Update a user
+        """
+        user = self.find_user_by(id=user_id)
+        for k, v in kwargs.items():
+            if k not in User.__table__.columns.keys():
+                raise ValueError
+            setattr(user, k, v)
+
+        self._session.commit()
