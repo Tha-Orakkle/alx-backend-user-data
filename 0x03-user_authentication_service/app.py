2b3b644745cfe35a9f5624877f1bfae263f58061
tha-orakkle <adegbiranayinoluwa.paul@yahoo.com>
2024-06-14 21:27:10 +0100

    destroy session

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,21 +2,22 @@
 """
 App Module
 """
-from flask import Flask, jsonify, request
 from auth import Auth
+from flask import abort, Flask, jsonify, request
+
 
 AUTH = Auth()
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET'], strict_slashes=False)
 def index():
     """Returns {"message": "Bienvenue"}
     """
     return jsonify({"message": "Bienvenue"})
 
 
-@app.route('/users', methods=['POST'])
+@app.route('/users', methods=['POST'], strict_slashes=False)
 def users():
     """
     Register User
@@ -29,6 +30,20 @@ def users():
                         "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login():
+    """login valid user and return session id
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if not AUTH.valid_login(email=email, password=password):
+        abort(401)
+    session_id = AUTH.create_session(email)
+    res = jsonify({"email": email, "message": "logged in"})
+    res.set_cookie("session_id", session_id)
+    return res
 
 
 if __name__ == "__main__":
--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -5,7 +5,9 @@ Authentication Module
 import bcrypt
 from db import DB
 from sqlalchemy.orm.exc import NoResultFound
+from typing import Optional
 from user import User
+import uuid
 
 
 def _hash_password(password: str) -> bytes:
@@ -13,6 +15,12 @@ def _hash_password(password: str) -> bytes:
     hashed_pwd = bcrypt.hashpw(password.encode('utf-8'),
                                bcrypt.gensalt())
     return hashed_pwd
+
+
+def _generate_uuid():
+    """Return a string representation of a new UUID
+    """
+    return str(uuid.uuid4())
 
 
 class Auth:
@@ -34,3 +42,40 @@ class Auth:
                                      hashed_password=hashed_password)
 
         return user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Validates Credentials"""
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(password.encode('utf-8'),
+                                  user.hashed_password)
+        except NoResultFound:
+            return False
+
+    def create_session(self, email: str) -> str:
+        """Create a session Id for a user
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
+        except NoResultFound:
+            return None
+
+    def get_user_from_session_id(self, session_id: str) -> Optional[User]:
+        """Gets user by the session id
+        """
+        try:
+            return self._db.find_user_by(session_id=session_id)
+        except NoResultFound:
+            return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """Destroys a session"""
+        try:
+            user = self._db.find_user_by(id=user_id)
+            self._db.update_user(user.id, session_id=None)
+            return None
+        except NoResultFound:
+            return None
